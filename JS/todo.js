1128b7b8987f75d9ec7b0af5834574f2bc519c9a
Narah Kim <84653078+knarah@users.noreply.github.com>
2021-11-22 14:56:34 -0500

    todos
    Saving todos in local storage
    and displaying saved todos

--- JS/todo.js
+++ JS/todo.js
@@ -1,22 +1,47 @@
 const toDoForm = document.querySelector(".todos_form");
 const toDoInput = toDoForm.querySelector("input");
 const toDoList = document.querySelector("#todos_list");
+let toDos =[];
+const savedTodos = localStorage.getItem('toDos');
+const parsedToDos = JSON.parse(savedTodos);
 
+//3. save toDos in local storage
+function saveToDos(inputValue){
+    localStorage.setItem('toDos', JSON.stringify(toDos));
 
-// get input value
+}
+
+//4. Display saved todo list
+
+DisplaySavedTodos();
+//1. get input value
 
 function handleToDoSubmit(event){
     event.preventDefault();
     const inputValue = toDoInput.value;
     DisplayToDos(inputValue);
     toDoInput.value = null;
+    toDos.push(inputValue);
+    saveToDos();
 }
 
+function DisplaySavedTodos() {
+    console.log(savedTodos);
+    if(savedTodos) {
+        parsedToDos.forEach(DisplayToDos);
+        toDos = parsedToDos;
+        // for (let i = 0; i < parsedToDos.length; i++) {
+        //     DisplayToDos(parsedToDos[i]);
+        // }
+   }
+ }
+
 function DisplayToDos(value){
+    
     const li = document.createElement('Li');
     const span = document.createElement('span');
     const toDoValue = document.createTextNode(value); 
-    const deleteButton = document.createElement('button');
+    const deleteButton = document.createElement('button');    
     deleteButton.innerHTML = '<i class="fas fa-trash-alt"></i>';
     li.appendChild(span);
     li.appendChild(deleteButton);
@@ -27,8 +52,10 @@ function DisplayToDos(value){
 
 function deleteTodo(event){
     const oldTodo = event.target.parentNode;
-    oldTodo.remove();    
+    console.log(oldTodo); 
+    oldTodo.remove();   
 }
+    
 
 toDoForm.addEventListener('submit', handleToDoSubmit);
 
